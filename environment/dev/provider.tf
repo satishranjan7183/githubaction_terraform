2808f8c7b16b31317b7531e2e63ca8b755f7d0f9
Aryan Raj <aryanraj212015@gmail.com>
2025-09-30 18:15:59 +0530

    added backend

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -6,8 +6,13 @@ terraform {
     }
 
   }
+backend "azurerm" {
+  resource_group_name   = ""
+  storage_account_name  = ""
+  container_name        = ""
+  key                   = ""
 }
-
+}
 provider "azurerm" {
   features {}
   subscription_id = "38d52de4-2fb6-4a85-96fb-5ba22d363e4e"
