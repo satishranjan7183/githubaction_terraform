0c09b2dc04f4732ca779c5d06baff1ba806df4e0
Aryan Raj <aryanraj212015@gmail.com>
2025-09-30 20:15:59 +0530

    added

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -16,4 +16,7 @@ backend "azurerm" {
 provider "azurerm" {
   features {}
   subscription_id = "38d52de4-2fb6-4a85-96fb-5ba22d363e4e"
+  client_id       = "f72f924c-5aa6-4e12-936d-2890cea66164"
+  client_secret   = "Ik_8Q~BB57TJW8GMMbc2ExwfaN_DUb2NuS5qRcNf"
+  tenant_id       = "8839fa1a-72d2-4f19-8736-4c73cf13d7a6"
 }
