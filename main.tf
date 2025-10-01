ddea72203fad6eb58bfd46cd231bb8ed3fb7d19e
satishranjan7183 <143581126+satishranjan7183@users.noreply.github.com>
2025-10-01 10:35:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,7 @@ provider "azurerm" {
   features {}
   subscription_id = "38d52de4-2fb6-4a85-96fb-5ba22d363e4e"
 }
+resource "azurerm_resource_group" "demo" {
+  name     = "example-resources"
+  location = "West Europe"
+}
